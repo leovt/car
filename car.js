807202b729103c6300fa07139de52db7a558bf37
leovt <leonhard.vogt@gmx.ch>
2020-08-07 08:05:35 +0200

    added keybord control for fwd/bwd

--- car.js
+++ car.js
@@ -14,42 +14,45 @@ let distance=0;
 let car=document.getElementById('car');
 let frontwheel=document.getElementById('frontwheel');
 
-car.setAttribute('transform', `translate(${x},${y})rotate(${w/deg})`);
-frontwheel.setAttribute('transform', `rotate(${a/deg})`);
+function moveCar() {
+  while (Math.abs(distance) >= h) {
+    if (distance > 0) {
+      x += h*Math.cos(w+a);
+      y += h*Math.sin(w+a);
+      w += Math.asin(Math.sin(a) * h / wheelBase);
+      distance -= h;
+    }
+    else {
+      w -= Math.asin(Math.sin(a) * h / wheelBase);
+      x -= h*Math.cos(w+a);
+      y -= h*Math.sin(w+a);
+      distance += h;
+    }
+  }
+  car.setAttribute('transform', `translate(${x},${y})rotate(${w/deg})`);
+  frontwheel.setAttribute('transform', `rotate(${a/deg})`);
+}
+
+moveCar();
 
 function onMouseMove(evt) {
   let client = document.documentElement.getBoundingClientRect();
   let xRatio = (evt.clientX - client.left) / client.width;
 
-  if (xRatio < 0.1) {
+  if (xRatio < 0.3) {
     a = -maxDeflection;}
   else if (xRatio < 0.5) {
-    a = -maxDeflection * (0.5 - xRatio) / 0.4;}
-  else if (xRatio <= 0.9) {
-    a = maxDeflection * (xRatio - 0.5) / 0.4;}
+    a = -maxDeflection * (0.5 - xRatio) / 0.2;}
+  else if (xRatio <= 0.7) {
+    a = maxDeflection * (xRatio - 0.5) / 0.2;}
   else {
     a = maxDeflection;}
-  frontwheel.setAttribute('transform', `rotate(${a/deg})`);
 
   if (evt.buttons % 2 == 1) {
     distance += 40*speed*(lastY - evt.clientY)/client.height;
-    while (Math.abs(distance) >= h) {
-      if (distance > 0) {
-        x += h*Math.cos(w+a);
-        y += h*Math.sin(w+a);
-        w += Math.asin(Math.sin(a) * h / wheelBase);
-        distance -= h;
-      }
-      else {
-        w -= Math.asin(Math.sin(a) * h / wheelBase);
-        x -= h*Math.cos(w+a);
-        y -= h*Math.sin(w+a);
-        distance += h;
-      }
-    }
-    car.setAttribute('transform', `translate(${x},${y})rotate(${w/deg})`);
     lastY = evt.clientY;
   }
+  moveCar();
 }
 
 function onMouseDown(evt) {
@@ -59,5 +62,17 @@ function onMouseDown(evt) {
   }
 }
 
+function onKeyDown(evt) {
+  if (evt.key === 'ArrowUp'){
+    distance += 0.1;
+    moveCar();
+  }
+  else if (evt.key === 'ArrowDown'){
+    distance -= 0.1;
+    moveCar();
+  }
+}
+
 document.addEventListener('mousemove', onMouseMove);
 document.addEventListener('mousedown', onMouseDown);
+document.addEventListener('keydown', onKeyDown);
